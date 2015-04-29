4b171311cf4719f4330d8c5480ed0c7336e9a30b
pafisher <pafisher@uwaterloo.ca>
2015-04-29 14:41:50 -0400

    Full entity class

---
+++ entity.py
@@ -0,0 +1,8 @@
+class Entity:
+	def __init__(self, level, hp, maxhp, attack, defense, speed):
+		self.level = level
+		self.hp = hp
+		self.maxhp = maxhp
+		self.attack = attack
+		self.defense = defense
+		self.speed = speed
