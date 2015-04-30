88148f2c6dfaf51db6627f39c90a2bba27a941d0
Stephen <stephenfisher@hotmail.ca>
2015-04-30 17:15:01 -0400

    Basic Damage Calculations

--- battle.py
+++ battle.py
@@ -19,35 +19,46 @@ class Battle:
 		if (move1 == "attack"):
 			if (move2 == "attack"):
 				if (self.fighter1.speed > self.fighter2.speed):
-					#Damage Calculation
+					#Fighter 1 attacks
 					db(dbflag, "Fighter 1 attacks.")
-					self.fighter2.hp -= self.fighter1.attack
+					self.fighter2.hp -= (self.fighter2.defense - self.fighter1.attack)/2
 					db(dbflag, "Fighter 2 HP: " + str(self.fighter2.hp))
+					#If Fighter 2 dies
 					if (self.fighter2.hp <= 0):
+						self.fighter2.hp = 0
 						db(dbflag, "Fighter 2 died")
 						self.fighter1.give_exp(50)
 						return
+					#Fighter 2 attacks
 					else:
 						db(dbflag, "Fighter 2 attacks.")
-						self.fighter1.hp -= self.fighter2.attack
+						self.fighter1.hp -= (self.fighter1.defense - self.fighter2.attack)/2
 						db(dbflag, "Fighter 1 HP: " + str(self.fighter1.hp))
+						#If Fighter 1 dies
 						if (self.fighter1.hp <= 0):
+							self.fighter1.hp = 0
 							db(dbflag, "Fighter 1 died")
 							self.fighter2.give_exp(50)
 							return 
 				else:
-					#Damage Calculation
+					#Fighter 2 attacks
 					db(dbflag, "Fighter 2 attacks.")
-					self.fighter1.hp -= self.fighter2.attack
+					self.fighter1.hp -= (self.fighter1.defense - self.fighter2.attack)/2
 					db(dbflag, "Fighter 1 HP: " + str(self.fighter1.hp))
+					#If Fighter 1 dies
 					if (self.fighter1.hp <= 0):
+						self.fighter1.hp = 0
+						db(dbflag, "Fighter 1 died.")
 						self.fighter2.give_exp(50)
 						return
 					else:
+						#Fighter 1 attacks
 						db(dbflag, "Fighter 1 attacks.")
-						self.fighter2.hp -= self.fighter1.attack
+						self.fighter2.hp -= (self.fighter2.defense - self.fighter1.attack)/2
 						db(dbflag, "Fighter 2 HP: " + str(self.fighter2.hp))
+						#If Fighter 2 dies
 						if (self.fighter2.hp <= 0):
+							self.fighter2.hp = 0
 							db(dbflag, "Fighter 2 died.")
 							self.fighter1.give_exp(50)
 							return
